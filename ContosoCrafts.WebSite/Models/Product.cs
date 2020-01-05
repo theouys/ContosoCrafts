4a53ef9f3035c536e02b64e12cc96b727a9e6413
Theo Uys <theouys@outlook.com>
2020-01-05 21:52:37 +0200

    Added surname

---
+++ ContosoCrafts.WebSite/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace ContosoCrafts.WebSite.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Maker { get; set; }
+        public string Title { get; set; }
+
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+
+        //Shortversion of the below is
+        //public override string ToString() => JsonSerializer.Serialize<Product>(this);
+        
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize<Product>(this);
+        }
+    }
+}
---
+++ ContosoCrafts.WebSite/Services/JsonFileProductService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using ContosoCrafts.WebSite.Models;
+using System.Text.Json;
+
+namespace ContosoCrafts.WebSite.Services
+{
+    public class JsonFileProductService
+    {
+        public IWebHostEnvironment WebHostEnvironment { get; }
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        private string JsonFileName
+        {
+            get {
+                return Path.Combine(WebHostEnvironment.WebRootPath,"data","product.json");
+                }
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(), 
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            }
+        }
+    }
+}
